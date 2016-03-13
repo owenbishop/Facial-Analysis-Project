070934a72f166f2bec473f69911bbb9867a5eb8a
owenbishop <owen.bishop32@gmail.com>
2016-03-13 14:49:23 +0000

    Update saveData.php

--- saveData.php
+++ saveData.php
@@ -23,6 +23,8 @@ if(!$selection)
 //gets the users unique email from session variable
 $email = $_SESSION['email'];
 
+
+
 //selects userid from the user table, relative to the email.
 $result = mysql_query("SELECT UserID FROM User WHERE Email_Address = '$email'");
 
@@ -38,6 +40,25 @@ $row = mysql_fetch_assoc($result);
 		$filteredData=substr($imageData, strpos($imageData, ",")+1);
 		//inserts to image table
 		$sql = mysql_query("insert into Image (UserID, Image) VALUES('$user', '$filteredData')");
+		$imgNo = mysql_query("Select ImageID from Image where UserID = '$user'");
+		
+		while($row = mysql_fetch_assoc($imgNo)){
+		$imgNo1 = $row['ImageID'];
+		}
+		
+		$png = ".png";
+		$imgName = "userimage/".$email.$imgNo1.$png;
+		
+		$imagePath = "C:/wamp/www/updated/".$imgName;
+		echo $imagePath;
+		
+		$imgPathQuery = mysql_query("insert into ImagePath (imagePath, UserID) VALUES('$imagePath','$user')");;
+		
+		$unencodedData = base64_decode($filteredData);
+		$fp = fopen($imgName, 'wb');
+		fwrite($fp, $unencodedData);
+		fclose(fp);
+		
 	}
 
 
